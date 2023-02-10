55cdaa56eec81f3f00207fe1ed86971551c92dc2
sariavaal <sariavaal@gmail.com>
2023-02-10 15:18:50 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,10 @@ export default defineConfig({
     server: {
         port: 5173,
         host: true,
-        https: true,
+        https: {
+            key: "/etc/letsencrypt/live/sosmujer.me/privkey.pem",
+            cert: "/etc/letsencrypt/live/sosmujer.me/fullchain.pem",
+        },
         strictPort: true,
         hmr: {
             host: '137.184.92.33',
